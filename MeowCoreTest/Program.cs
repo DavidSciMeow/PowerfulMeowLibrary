e74bb513d06a3757736f14aafc0255a24b4dad4f
DavidSciMeow <davidtennant405@outlook.com>
2022-02-23 19:19:49 +0800

    update on core data changes

--- MeowCoreTest/Program.cs
+++ MeowCoreTest/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Meow.Database.Mysql;
 
 namespace MeowCoreTest
 {
@@ -7,8 +6,17 @@ namespace MeowCoreTest
     {
 		public static void Main(string[] args)
         {
-
-		}
+            using var d = ReturnService();
+            var k = d.PrepareDb("SELECT qid," +
+                "rinkorushenable,rinkorushtype,rinkorushscore," +
+                "rinkorushdndtimestart,rinkorushdndtimeend" +
+                " FROM RinkoUser").GetDataSet();
+            System.Console.WriteLine(k);
+        }
+        public static MysqlDBH ReturnService()
+        {
+            return new MysqlDBH("rinko", "112.6.216.175", "12306", "rinkobot", "rinkobot");
+        }
     }
 }
 
