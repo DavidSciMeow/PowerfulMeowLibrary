0c59198c4aa695d1595c22701390663fb33e0a5a
DavidMeow <davidtennant405@outlook.com>
2022-06-12 13:13:45 +0800

    update

--- Meow.Weather/README.md
+++ Meow.Weather/README.md
@@ -2,6 +2,7 @@
 本组件包已经上传nuget, 请关注 nuget Electronicute.Weather 包  
 使用 Meow.Weather.CN.Interpreter.RectifyWord(string) string 为下面表格内某个"指令" 函数来获取识别类   
 使用 Meow.Weather.CN.Interpreter.RectifyWord(string\[\]) 列表省市顺序为 \[ 省 , 市 \] 函数来获取此位置的天气详细信息  
+
 | 功能                 | 指令      |
 |--------------------|---------|
 | 基本天气分析std.hpa中国    | cdwh0   |
--- MeowCoreTest/Program.cs
+++ MeowCoreTest/Program.cs
@@ -7,43 +7,6 @@ namespace MeowCoreTest
     {
 		public static void Main(string[] args)
         {
-            var k = from a in args where a != null && a.Length > 0 select a;
-            k.
-            new Program().Select().From("").Where("").OrderBy().GroupBy().Limit(1000)
-        }
-        //select entire table
-        public Program Select(params object[] columnSelection)
-        {
-            return this;
-        }
-        public Program From(string tableName)
-        {
-            return this;
-        }
-        public Program Where(string Caluse)
-        {
-            return this;
-        }
-        public Program OrderBy(params object[] columnSelection)
-        {
-            return this;
-        }
-        public Program GroupBy(params object[] columnSelection)
-        {
-            return this;
-        }
-        public Program Limit(long num)
-        {
-            return this;
-        }
-        //select table with some col.
-        public static void Select(string table, params object[] columnSelection)
-        {
-
-        }
-        //select table with limitation
-        public static void Select(string table, long limitation, params object[] columnSelection)
-        {
 
         }
     }
